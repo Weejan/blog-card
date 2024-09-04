47140926cc20002c8896e79b90e4e0e069e91577
weejan <weezan49@gmail.com>
2024-09-04 14:15:26 +0545

    Fix blog inner component layout

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -5,17 +5,21 @@ export default function Blog({ blog }) {
   return (
     <div className="max-w-[300px] h-[400px] border border-slate-400 m-6">
       <img src={img} />
-      <div className="px-2">
-        <h1 className="text-xl py-2">{title}</h1>
-        <p className="pb-6 text-sm">{des}</p>
+      <div className="px-2 flex flex-col justify-between h-[50%]">
+        <div>
+          <h1 className="text-xl py-2">{title}</h1>
+          <p className="pb-10 text-sm">{des}</p>
+        </div>
 
-        <a
-          href="#"
-          className="px-7 py-2 bg-green-500 rounded-full text-white hover:shadow-md font-medium
+        <div className="mb-4">
+          <a
+            href="#"
+            className="px-7 py-2 bg-green-500 rounded-full text-white hover:shadow-md font-medium
         "
-        >
-          Read More
-        </a>
+          >
+            Read More
+          </a>
+        </div>
       </div>
     </div>
   );
